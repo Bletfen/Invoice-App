3e2e655ea411a7bbd5c971f28e72b6f09b8e8dce
Giorgi <giorgigrigolishvili@gmail.com>
2025-08-20 23:57:21 +0400

    basic styles for main invoices page

--- src/pages/Invoices.tsx
+++ src/pages/Invoices.tsx
@@ -45,17 +45,83 @@ export default function Invoices() {
       <div className="flex flex-col">
         {dataBase.map((item) => (
           <Link key={item.id} to={item.id}>
-            <div>
-              <div>
+            <div
+              className="pt-[2.4rem] pb-[2.2rem]
+              px-[2.4rem]"
+            >
+              <div className="flex">
                 <div>
-                  <h2>{item.id}</h2>
-                  <p>{item.createdAt}</p>
+                  <h2
+                    className="text-[1.5rem]
+                    font-bold leading-[1.5rem]
+                    tracking-[-0.25px]
+                    text-invoiceHeaderText-light"
+                  >
+                    <span className="text-[#7e88c3]">#</span>
+                    {item.id}
+                  </h2>
+                  <p
+                    className="text-[1.3rem]
+                    font-[500] leading-[1.5rem]
+                    tracking-[-0.1px]
+                    text-invoicePText-light
+                    hidden"
+                  >
+                    <span className="text-[#888eb0]">Due </span>
+                    {item.createdAt}
+                  </p>
                 </div>
-                <span>{item.clientName}</span>
+                <span
+                  className="text-[1.3rem]
+                    font-[500] leading-[1.5rem]
+                    tracking-[-0.1px]
+                    text-invoicePText-light"
+                >
+                  {item.clientName}
+                </span>
               </div>
-              <div>
-                <p>£{item.total}</p>
-                <button>{item.status}</button>
+              <div
+                className="flex
+                items-center"
+              >
+                <div className="flex flex-col gap-[0.9rem]">
+                  <p
+                    className="text-[1.3rem]
+                    font-[500] leading-[1.5rem]
+                    tracking-[-0.1px]
+                    text-invoicePText-light"
+                  >
+                    <span className="text-[#888eb0]">Due </span>
+                    {item.createdAt}
+                  </p>
+                  <p
+                    className="text-[1.5rem]
+                    font-bold leading-[2.4rem]
+                    tracking-[-0.25px]
+                    text-invoiceHeaderText-light"
+                  >
+                    £{item.total}
+                  </p>
+                </div>
+                <button
+                  className={`px-[3rem] pt-[1.4rem] pb-[1.1rem]
+                  bg-paidButton-light rounded-[0.6rem]
+                  text-[1.5rem] font-bold leading-[1.5rem]
+                  tracking-[-0.25px]
+                  flex items-center ${
+                    item.status.toLowerCase() === "paid"
+                      ? "bg-paidButton-light"
+                      : item.status.toLowerCase() === "pending"
+                      ? "bg-pendingButton-light"
+                      : "bg-draftButton-light"
+                  }`}
+                >
+                  <div
+                    className="w-[0.8rem] h-[0.8rem] 
+                    bg-black rounded-full"
+                  ></div>
+                  {item.status}
+                </button>
               </div>
             </div>
           </Link>

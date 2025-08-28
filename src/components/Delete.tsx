bc0f34280348b18e714eed0fcaab236bc841d789
Giorgi <giorgigrigolishvili@gmail.com>
2025-08-28 20:02:59 +0400

    polished delete component added dark mode

--- src/components/Delete.tsx
+++ src/components/Delete.tsx
@@ -18,6 +18,10 @@ export default function Delete({
       className="absolute pt-[3.4rem] pb-[3.2rem]
         px-[3.2rem] bg-white rounded-[0.8rem]
         shadow-[0_10px_10px_-10px_rgba(72,84,159,0.10)]
+        transition-all duration-300 dark:bg-[#132139]
+        w-[32.7rem] md:w-[48rem]
+        md:pt-[5.1rem] md:px-[4.8rem]
+        md:pb-[4.8rem]
         "
     >
       <h1
@@ -25,7 +29,9 @@ export default function Delete({
         font-bold leading-[3.2rem]
         tracking-[-0.5px]
         text-[#0c0e16]
-        mb-[0.8rem]"
+        mb-[0.8rem]
+        md:mb-[1.2rem]
+        transition-all duration-300 dark:text-white"
       >
         Confirm Deletion
       </h1>
@@ -33,14 +39,17 @@ export default function Delete({
         className="text-[1.3rem]
         font-[500] leading-[2.2rem]
         tracking-[-0.1px]
-        text-[#888eb0] mb-[2.2rem]"
+        text-[#888eb0] mb-[2.2rem]
+        md:mb-[1.4rem]
+        transition-all duration-300 dark:text-[#dfe3fa]"
       >
-        Are you sure you want to delete invoice #{invoiceId}? <br />
-        This action cannot be undone.
+        Are you sure you want to delete invoice #{invoiceId}? This action cannot
+        be undone.
       </p>
       <div
         className="flex
-        justify-end"
+        justify-end
+        gap-[0.8rem]"
       >
         <button
           type="button"
@@ -49,7 +58,9 @@ export default function Delete({
           bg-[#f9fafe] rounded-[2.4rem]
           text-[1.5rem] font-bold leading-[1.5rem]
           tracking-[-0.25px] text-[#7e88c3]
-          cursor-pointer"
+          cursor-pointer
+          transition-all duration-300 dark:bg-[#252945]
+          dark:text-[#dfe3fa]"
           onClick={() => setShowDelete(false)}
         >
           Cancel
